b0d85c247ed6e6c99b04a42ea86784f8385df2bd
applojadev <alexandreoliveirasouza104@gmail.comm>
2023-02-14 09:48:49 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-00874d747dde814fa"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "devBuild"
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+region = "us-east-1"
+access_key = "<informe aqui>"
+secret_key = "<informe aqui>"
+exemplo_int = 30
